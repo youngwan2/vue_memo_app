4a1df5c43c4d8071eacaf89c33db066918836e6f
KYW <qodna25@gmail.com>
2023-06-03 17:55:32 +0900

    main.vue 컴포지션 api 중 reactive 사용 실습

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,24 +1,40 @@
 <template>
   <main class="main_memo">
+    <input type="text" @input="valueAdd($event.target.value)" />
+    <button @click="add(input)">추가</button>
     <ol>
-      <li v-for="(memo,i) in data" :key="i">{{ memo }}</li>
+      <li v-for="(memo, i) in data" :key="i">{{ memo }}</li>
     </ol>
   </main>
 </template>
 
 <script>
+import { reactive } from "vue";
 export default {
   name: "MainVue",
   setup() {
-    const data = [
+    const data = reactive([
       "메모내용1",
       "메모내용2",
       "메모내용3",
       "메모내용4",
       "메모내용5",
-      "메모내용6",
-    ];
-    return {data}
+    ]);
+
+    const input = "";
+    return { data, input };
+  },
+
+  methods: {
+    // 사용자가 입력창에 텍스트를 입력하면 전달받은 값을 input에 저장함
+    valueAdd(value) {
+      this.input = value;
+    },
+    add(value) {
+      console.log(value);
+      this.data.push(value);
+      console.log(this.data);
+    },
   },
 };
 </script>
